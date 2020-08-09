8ac5ea94bc5f62a223196f41461bea70869f8d2c
Kentaro Takasaki <ktakasaki@pantarhei.jp.net>
2020-08-09 16:14:18 +0900

    total検索

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,24 +1,28 @@
 class StatusesController < ApplicationController
     def index
-        @statuses=Status.all
+        if params[:ttl_max] && params[:ttl_max]
+            @statuses= Status.where(" ? <= (attack + defence) AND (attack + defence) <= ?", params[:ttl_min].to_i, params[:ttl_max].to_i).all
+        else
+            @statuses=Status.all
+        end
         @stars=Star.all
     end
-    
+
     def create
         Status.create(name:params["monster"]["name"],attack:params["monster"]["attack"],defence:params["monster"]["defence"],star_id:params["monster"]["star_id"])
         redirect_to"/"
     end
-    
+
     def destroy
         status=Status.find(params[:id])
         status.destroy
         redirect_to"/"
     end
-    
+
     def edit
         @status=Status.find(params[:id])
     end
-    
+
     def update
         status=Status.find(params[:id])
         status.name=params["monster"]["name"]
